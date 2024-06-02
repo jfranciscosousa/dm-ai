db86e5d9726edf362e4c5d0b3f7b0054961781b8
Francisco Sousa <francisco.sousa@hey.com>
2024-06-02 18:26:43 +0100

    Performance

--- src/app/(authenticated)/npc/page.tsx
+++ src/app/(authenticated)/npc/page.tsx
@@ -1,7 +1,6 @@
 import NPC from "@/components/NPC";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { redirect, RedirectType } from "next/navigation";
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -11,24 +10,14 @@ export default async function NpcPage({ searchParams }: Props) {
   const search =
     typeof searchParams.search === "string" ? searchParams.search : undefined;
   const uuid =
-    typeof searchParams.uuid === "string"
-      ? searchParams.uuid
-      : crypto.randomUUID();
-
-  async function action(formData: FormData) {
-    "use server";
-
-    const search = formData.get("search") as string;
-    const uuid = crypto.randomUUID();
-
-    redirect(`/npc?search=${search}&uuid=${uuid}`);
-  }
+    typeof searchParams.uuid === "string" ? searchParams.uuid : undefined;
+  const newUuid = crypto.randomUUID();
 
   return (
     <div className="p-4 h-full flex flex-col">
       <h1 className="font-bold text-2xl mb-4">NPC generator</h1>
 
-      <form action={action} className="mb-8 flex gap-2">
+      <form className="mb-8 flex gap-2">
         <Input
           name="search"
           defaultValue={search}
@@ -36,11 +25,13 @@ export default async function NpcPage({ searchParams }: Props) {
           placeholder="A strong dwarf warrior..."
         />
 
+        <input type="hidden" defaultValue={newUuid} name="uuid" />
+
         <Button>Generate</Button>
       </form>
 
       <div className="flex-grow">
-        {search && <NPC prompt={search} uuid={uuid} />}
+        {search && uuid && <NPC prompt={search} uuid={uuid} />}
       </div>
     </div>
   );
--- src/app/(authenticated)/tavern/page.tsx
+++ src/app/(authenticated)/tavern/page.tsx
@@ -11,35 +11,28 @@ export default async function TavernPage({ searchParams }: Props) {
   const search =
     typeof searchParams.search === "string" ? searchParams.search : undefined;
   const uuid =
-    typeof searchParams.uuid === "string"
-      ? searchParams.uuid
-      : crypto.randomUUID();
-
-  async function action(formData: FormData) {
-    "use server";
-
-    const search = formData.get("search") as string;
-    const uuid = crypto.randomUUID();
-
-    redirect(`/tavern?search=${search}&uuid=${uuid}`);
-  }
+    typeof searchParams.uuid === "string" ? searchParams.uuid : undefined;
+  const newUuid = crypto.randomUUID();
 
   return (
     <div className="p-4 h-full flex flex-col">
       <h1 className="font-bold text-2xl mb-4">Tavern generator</h1>
 
-      <form action={action} className="mb-8 flex gap-2">
+      <form className="mb-8 flex gap-2">
         <Input
           name="search"
           defaultValue={search}
           className="flex-grow"
           placeholder="A beautiful inn in lazy elvish town..."
         />
+
+        <input type="hidden" defaultValue={newUuid} name="uuid" />
+
         <Button>Generate</Button>
       </form>
 
       <div className="flex-grow">
-        {search && <Tavern uuid={uuid} prompt={search} />}
+        {search && uuid && <Tavern prompt={search} uuid={uuid} />}
       </div>
     </div>
   );
