77b8026863f25f751c26b2ee4d60ecea4b433fb1
Francisco Sousa <francisco.sousa@hey.com>
2025-03-24 23:24:11 +0000

    fix

--- src/app/(authenticated)/npc/page.tsx
+++ src/app/(authenticated)/npc/page.tsx
@@ -2,14 +2,15 @@ import GeneratorShell from "@/components/GeneratorShell";
 import NPC from "@/components/NPC";
 
 type Props = {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export default async function NpcPage({ searchParams }: Props) {
+  const searchUnparsed = (await searchParams).search;
+  const uuidUnparsed = (await searchParams).uuid;
   const search =
-    typeof searchParams.search === "string" ? searchParams.search : undefined;
-  const uuid =
-    typeof searchParams.uuid === "string" ? searchParams.uuid : undefined;
+    typeof searchUnparsed === "string" ? searchUnparsed : undefined;
+  const uuid = typeof uuidUnparsed === "string" ? uuidUnparsed : undefined;
 
   return (
     <GeneratorShell
--- src/app/(authenticated)/shop/page.tsx
+++ src/app/(authenticated)/shop/page.tsx
@@ -2,14 +2,15 @@ import GeneratorShell from "@/components/GeneratorShell";
 import Shop from "@/components/Shop";
 
 type Props = {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export default async function ShopPage({ searchParams }: Props) {
+  const searchUnparsed = (await searchParams).search;
+  const uuidUnparsed = (await searchParams).uuid;
   const search =
-    typeof searchParams.search === "string" ? searchParams.search : undefined;
-  const uuid =
-    typeof searchParams.uuid === "string" ? searchParams.uuid : undefined;
+    typeof searchUnparsed === "string" ? searchUnparsed : undefined;
+  const uuid = typeof uuidUnparsed === "string" ? uuidUnparsed : undefined;
 
   return (
     <GeneratorShell
